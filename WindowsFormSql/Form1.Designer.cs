7ad24fb84c65cc2149a071ec649a23c1e07d91a0
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2023-03-07 23:21:02 -0300

    Add project files.

---
+++ WindowsFormSql/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.campoNome = new System.Windows.Forms.TextBox();
+            this.select = new System.Windows.Forms.Button();
+            this.listRegister = new System.Windows.Forms.ListView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(197, 201);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(109, 37);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // campoNome
+            // 
+            this.campoNome.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.campoNome.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.campoNome.Location = new System.Drawing.Point(267, 145);
+            this.campoNome.Name = "campoNome";
+            this.campoNome.Size = new System.Drawing.Size(203, 20);
+            this.campoNome.TabIndex = 1;
+            this.campoNome.TextChanged += new System.EventHandler(this.campoNome_TextChanged);
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(312, 201);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(109, 37);
+            this.select.TabIndex = 2;
+            this.select.Text = "Consultar";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // listRegister
+            // 
+            this.listRegister.HideSelection = false;
+            this.listRegister.Location = new System.Drawing.Point(240, 270);
+            this.listRegister.Name = "listRegister";
+            this.listRegister.Size = new System.Drawing.Size(241, 97);
+            this.listRegister.TabIndex = 4;
+            this.listRegister.UseCompatibleStateImageBehavior = false;
+            this.listRegister.SelectedIndexChanged += new System.EventHandler(this.listRegister_SelectedIndexChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(426, 201);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 37);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listRegister);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.campoNome);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox campoNome;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.ListView listRegister;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ WindowsFormSql/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            listRegister.View = View.Details;
+            listRegister.LabelEdit = true;
+            listRegister.AllowColumnReorder = true;
+            listRegister.FullRowSelect = true;
+            listRegister.GridLines = true;
+
+            listRegister.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            listRegister.Columns.Add("Nome", 207, HorizontalAlignment.Left);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=1234567;database=db_agenda";
+                //string data_source = "server=localhost;password=1234567;user=root;database=db_agenda"; SqlServer conexao
+                string nome = campoNome.Text;
+
+                MySqlConnection conexao = new MySqlConnection(data_source);
+
+                string sql = "INSERT INTO contatos (nome) VALUES ('" + nome + "')";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                conexao.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Registro inserido com sucesso");
+
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private void select_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sql = string.Empty;
+                string data_source = "datasource=localhost;username=root;password=1234567;database=db_agenda";
+                //string data_source = "server=localhost;password=1234567;user=root;database=db_agenda"; SqlServer conexao
+                string nome = campoNome.Text;
+
+                MySqlConnection conexao = new MySqlConnection(data_source);
+
+                conexao.Open();
+
+                if (campoNome.Text == "")
+                    sql = "select * from contatos";
+                else
+                {
+                    sql = $"select * from contatos where nome like '%{campoNome.Text}%'";
+                }
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                listRegister.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        reader.GetString(0),
+                        reader.GetString(1)
+                    };
+                    var register = new ListViewItem(row);
+
+                    listRegister.Items.Add(register);
+                }
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int itemId = int.Parse(listRegister.SelectedItems[0].Text);
+                string data_source = "datasource=localhost;username=root;password=1234567;database=db_agenda";
+                //string data_source = "server=localhost;password=1234567;user=root;database=db_agenda"; SqlServer conexao
+                string nome = campoNome.Text;
+
+                MySqlConnection conexao = new MySqlConnection(data_source);
+
+                string sql = $"delete from contatos where id = {itemId}";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                conexao.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Registro excluido com sucesso");
+
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+        private void campoNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listRegister_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
